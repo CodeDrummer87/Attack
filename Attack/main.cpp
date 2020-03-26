5a17fad5c5d8ce724d964cb1b7bcdf57cc332800
CodeDrummer <code.drummer87@gmail.com>
2020-03-27 01:42:42 +0600

    Link tank movement to time

--- Attack/main.cpp
+++ Attack/main.cpp
@@ -13,6 +13,12 @@ int main()
 	int sizeX = GetSystemMetrics(SM_CXFULLSCREEN);
 	int sizeY = GetSystemMetrics(SM_CYFULLSCREEN);
 
+	if (sizeX > 1500 && sizeX < 1920)
+	{
+		sizeX = 1920;
+		sizeY = 1080;
+	}
+
 	RenderWindow app(VideoMode(sizeX, sizeY), "Test", Style::Fullscreen);
 
 	Texture t;
@@ -25,8 +31,15 @@ int main()
 
 	float currentFrame = 0;
 
+	Clock clock;
+
 	while (app.isOpen())
 	{
+		float time = clock.getElapsedTime().asMicroseconds();
+		clock.restart();
+
+		time /= 1700;
+
 		Event event;
 		while (app.pollEvent(event))
 		{
@@ -36,36 +49,36 @@ int main()
 
 		if (Keyboard::isKeyPressed(Keyboard::Left))
 		{
-			s.move(-0.1, 0);
+			s.move(-0.1 * time, 0);
 
-			currentFrame += 0.05;
+			currentFrame += 0.05 * time;
 			if (currentFrame > 2) currentFrame -= 2;
 			s.setTextureRect(IntRect(64 + 64, 64 * int(currentFrame), -64, 64));
 		}
 
 		if(Keyboard::isKeyPressed(Keyboard::Right))
 		{
-			s.move(+0.1, 0);
+			s.move(+0.1 * time, 0);
 
-			currentFrame += 0.05;
+			currentFrame += 0.05 * time;
 			if (currentFrame > 2) currentFrame -= 2;
 			s.setTextureRect(IntRect(64, 64 * int(currentFrame), 64, 64));
 		}
 
 		if (Keyboard::isKeyPressed(Keyboard::Up))
 		{
-			s.move(0, -0.1);
+			s.move(0, -0.1 * time);
 
-			currentFrame += 0.05;
+			currentFrame += 0.05 * time;
 			if (currentFrame > 2) currentFrame -= 2;
 			s.setTextureRect(IntRect(0, 64 * int(currentFrame), 64, 64));
 		}
 
 		if (Keyboard::isKeyPressed(Keyboard::Down))
 		{
-			s.move(0, +0.1);
+			s.move(0, +0.1 * time);
 
-			currentFrame += 0.05;
+			currentFrame += 0.05 * time;
 			if (currentFrame > 2) currentFrame -= 2;
 			s.setTextureRect(IntRect(0, 64 * int(currentFrame) + 64, 64, -64));
 		}
